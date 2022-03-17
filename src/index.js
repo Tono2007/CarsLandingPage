332dcc688aaca1f3616b1c32944adb6b436af5b2
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2022-03-17 16:36:27 -0600

    [ADD] Featured Section Base
    -Swiper base cdn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "@styles/style.scss";
 import "./utils/navbarHandler";
 import "./utils/loginModalHandler";
 import "./utils/homeParallax";
-
+import "./utils/swipershandlers";
 
 function clg(text) {
   console.log(text);
