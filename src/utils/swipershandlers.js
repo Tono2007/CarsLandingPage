7c40123f3c1839ff46399a4a6f8a697e8054722a
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2022-03-18 12:26:34 -0600

    [UPDATE] Swiper install from NPM

--- src/utils/swipershandlers.js
+++ src/utils/swipershandlers.js
@@ -1,28 +1,34 @@
-
+//import "swiper/css";
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from "swiper";
 
 var swiper = new Swiper(".vehicles__slider", {
-    grabCursor: true,
-    centeredSlides: true,  
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-      delay: 9500,
-      disableOnInteraction: false,
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  modules: [Navigation, Pagination],
+  pagination: {
+    el: ".vehicles__slider__pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".vehicles__slider__navigation--next",
+    prevEl: ".vehicles__slider__navigation--prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable:true,
+    768: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
+    1024: {
+      slidesPerView: 3,
     },
-  });
-  
+  },
+});
